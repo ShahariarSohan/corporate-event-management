96ec130f3c016d7296ac9d4bbc9692dca8030f94
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-04-21 15:58:42 +0600

    why choose us section added

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -1,7 +1,64 @@
 import React from "react";
-
+import banner1 from "../assets/banner/banner1.jpg";
+import banner2 from "../assets/banner/banner2.jpg";
+import banner3 from "../assets/banner/banner3.jpg";
 const WhyChooseUs = () => {
-  return <div className="p-10"></div>;
+  return (
+    <div className="p-10">
+      <h3 className="text-center font-bold text-4xl md:mb-5 lg:mb-10">
+        Why Choose Us
+      </h3>
+      <div className="hero lg:mb-20">
+        <div className="hero-content flex-col lg:flex-row">
+          <img src={banner1} className="md:max-w-sm rounded-lg shadow-2xl" />
+          <div className="text-center lg:text-end">
+            <h1 className="text-4xl font-bold">
+              Enhanced Reach and Accessibility
+            </h1>
+            <p className="py-6">
+              Digital event organizers can reach a global audience, eliminating
+              geographical barriers and enabling wider participation. Digital
+              event organizers can reach a global audience, eliminating
+              geographical barriers and enabling wider participation. .
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="hero lg:mb-20">
+        <div className="hero-content flex-col-reverse lg:flex-row">
+          <div className="text-center lg:text-start">
+            <h1 className="text-4xl font-bold">
+              Interactive and Engaging Experiences
+            </h1>
+            <p className="py-6">
+              Digital events can incorporate interactive features like live Q&A
+              sessions, polls, breakout rooms, and gamification to enhance
+              attendee engagement.Digital events can incorporate interactive
+              features like live Q&A sessions, polls, breakout rooms, and
+              gamification to enhance attendee engagement.
+            </p>
+          </div>
+          <img src={banner2} className="md:max-w-sm rounded-lg shadow-2xl" />
+        </div>
+      </div>
+      <div className="hero lg:mb-20">
+        <div className="hero-content flex-col lg:flex-row">
+          <img src={banner3} className="md:max-w-sm rounded-lg shadow-2xl" />
+          <div className="text-center lg:text-end">
+            <h1 className="text-4xl font-bold">
+              Customized and Scalable Solutions
+            </h1>
+            <p className="py-6">
+              Digital event organizers can tailor events to specific needs and
+              scale them up or down based on demand.Digital event organizers can
+              tailor events to specific needs and scale them up or down based on
+              demand.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WhyChooseUs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../components/Banner";
 import HowWeWorks from "../components/HowWeWorks";
 import AboutUs from "../components/AboutUs";
+import WhyChooseUs from "../components/WhyChooseUs";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner></Banner>
       <AboutUs></AboutUs>
       <HowWeWorks></HowWeWorks>
+      <WhyChooseUs></WhyChooseUs>
     </div>
   );
 };
